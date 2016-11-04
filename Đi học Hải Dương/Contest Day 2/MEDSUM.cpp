da179b6a599363a3bf613ad11bc649c39e314bfc
Tran Minh Hieu <tranhieudev23@gmail.com>
2016-11-04 16:43:30 +0700

    Optimized MEDSUM, now can get AC

--- Đi học Hải Dương/Contest Day 2/MEDSUM.cpp
+++ Đi học Hải Dương/Contest Day 2/MEDSUM.cpp
@@ -10,17 +10,17 @@
 #define mp make_pair
 
 using namespace std;
-int n, l, mem = 0, a[mn][mn2], pos[mn][mn2], r[mn];
-int val[mn * mn2];
+int n, l, mem = 0, a[mn][mn2], r[mn], num[mn], numNum[mn2];
+int val[mn * mn2], id[mn * mn2];
 priority_queue< pair<int, int> > p;
 
 void setup()
 {
-    cin >> n >> l;
+    scanf("%d%d", &n, &l);
     FOR(i, 1, n)
     {
         FOR(j, 1, l)
-            cin >> a[i][j];
+            scanf("%d", &a[i][j]);
         p.push(mp(- a[i][1], i));
     }
 }
@@ -32,58 +32,37 @@ void merging()
         int u = p.top().S;
         int w = - p.top().F;
         p.pop();
-        pos[u][++ r[u]] = ++ mem;
+        r[u] ++, mem ++;
         val[mem] = w;
+        id[mem] = u;
         if (r[u] == l)
             continue;
         p.push(mp(- a[u][r[u] + 1], u));
     }
 }
 
-int getNum(int x, int u)
-{
-    int lef = 0, rig = l + 1;
-    while(rig - lef > 1)
-    {
-        int mid = ((rig + lef) >> 1);
-        if (pos[u][mid] <= x)
-            lef = mid;
-        else
-            rig = mid;
-    }
-    return lef;
-}
-
-int getAns(int x, int y)
-{
-    int lef = 0, rig = mem;
-    while(rig - lef > 1)
-    {
-        int mid = ((lef + rig) >> 1);
-        int num = getNum(mid, x) + getNum(mid, y);
-        if (num < l)
-            lef = mid;
-        else
-            rig = mid;
-    }
-    return val[rig];
-}
-
 void xuly()
 {
     merging();
     long long ans = 0;
-    FOR(i, 1, n - 1)
-    FOR(j, i + 1, n)
-        ans = (ans + getAns(i, j)) % maxC;
-    cout << ans;
+    numNum[0] = n;
+    FOR(i, 1, mem)
+    {
+        int u = id[i];
+        numNum[num[u]] --;
+        num[u] ++;
+        numNum[num[u]] ++;
+        long long good = numNum[l - num[u]];
+        if (num[u] == l - num[u])
+            good --;
+        ans += good * val[i];
+    }
+    ans = ((ans % maxC) + maxC) % maxC;
+    printf("%I64d", ans);
 }
 
 int main()
 {
-    ios_base::sync_with_stdio(0);
-    cin.tie(NULL);
-    cout.tie(NULL);
     freopen("MEDSUM.INP", "r", stdin);
     freopen("MEDSUM.OUT", "w", stdout);
     setup ();
